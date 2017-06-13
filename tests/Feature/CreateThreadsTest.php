2aff0067a7803404ca5c8091df792dbbf6e42621
Carlos Montezano Lopes <cmontezanol@gmail.com>
2017-06-12 23:07:35 -0300

    Lesson 8

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -18,6 +18,13 @@ class CreateThreadsTest extends TestCase
         $this->post('/threads', $thread->toArray());        
     }
 
+    public function testGuestsCannotSeeTheCreateThreadPage()
+    {
+        $this->withExceptionHandling()
+            ->get('/threads/create')
+            ->assertRedirect('/login');
+    }
+
     public function testAnAuthenticatedUserCanCreateNewForumThreads()
     {
         $this->signIn();
